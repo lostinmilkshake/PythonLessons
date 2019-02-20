c123f010cce8636d507c2bc71fe26b91fd7c5f89
Dina The Dinosaur <kirillitvinov@gmail.com>
2019-02-20 19:53:30 +0300

    Update 2

--- Python_Step_2/2.2.2py.py
+++ Python_Step_2/2.2.2py.py
@@ -1,1 +1,13 @@
-import simplecrypt 
+import simplecrypt
+
+encrypted = open("encrypted.bin", "rb").read()
+passwords = open("passwords.txt").readlines()
+
+for p in passwords:
+    p = p.strip()
+    try:
+        s = simplecrypt.decrypt(p, encrypted)
+    except simplecrypt.DecryptionException:
+        continue
+
+    print(s.decode("utf-8"))
